1ea9db95cf90e8467c6c3dc8fdf882d95fdcda14
Jeremy Puchta <hello@jeremypuchta.com>
2022-09-28 12:52:37 +0200

    Create 49_Group-Anagrams.java

---
+++ 49_Group-Anagrams.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> groupedAnagrams = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String s : strs) {
+            char[] hash = new char[26];
+            for (char c : s.toCharArray()) {
+                hash[c - 'a']++;
+            }
+            String key = new String(hash);
+            map.computeIfAbsent(key, k -> new ArrayList<>());
+            map.get(key).add(s);
+        }
+        
+        groupedAnagrams.addAll(map.values());
+        return groupedAnagrams;
+    }
+}
