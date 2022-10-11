4a514a2342f9f5c9cb448e983514edb803e37555
Jeremy Puchta <hello@jeremypuchta.com>
2022-10-11 15:38:36 +0200

    Create 424_Longest-Repeating-Character-Replacement.java

---
+++ 424_Longest-Repeating-Character-Replacement.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        Map<Character, Integer> count = new HashMap<>();
+        int result = 0;
+        
+        int l = 0;
+        for (int r = 0; r < s.length(); r++) {
+            count.put(s.charAt(r), count.getOrDefault(s.charAt(r), 0) + 1);
+            while ((r - l + 1) - Collections.max(count.values()) > k) {
+                count.put(s.charAt(l), count.get(s.charAt(l)) - 1);
+                l++;
+            }
+            result = Math.max(result, (r - l + 1));
+        }
+        
+        return result;
+    }
+}
