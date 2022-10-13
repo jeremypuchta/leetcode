7fc2a437f56237236406b90cebb2cb0f664a8ed1
Jeremy Puchta <hello@jeremypuchta.com>
2022-10-13 22:45:00 +0200

    Update 20_Valid-Parentheses.java

--- 20_Valid-Parentheses.java
+++ 20_Valid-Parentheses.java
@@ -1,31 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
-        if (s.startsWith("]") || s.startsWith(")") || s.startsWith("}")) return false;
-        
         Map<Character, Character> map = new HashMap<>();
         map.put(')', '(');
         map.put(']', '[');
         map.put('}', '{');
         
         Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char bracket = s.charAt(i);
-            if (bracket == '(' || bracket == '[' || bracket == '{') {
-                stack.push(bracket);
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                if (map.get(bracket) != stack.pop()) {
-                    return false;
-                }
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else if (stack.size() == 0 || map.get(c) != stack.pop()) {
+                return false;
             }
         }
         
-        if (!stack.isEmpty()) {
-            return false;
-        }
-        
-        return true;
+        return stack.size() == 0;
     }
 }
